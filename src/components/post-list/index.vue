67448cbfca650c1a8d00d6d07c824b5d252c4694
kongjun0320 <1741142595@qq.com>
2021-12-18 21:30:57 +0800

    feat: add post-list

---
+++ src/components/post-list/index.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="post-list">
+    <article v-for="post in posts" :key="post._id" class="card mb-3 shadow-sm">
+      <div class="card-body">
+        <h4>
+          <router-link :to="`/posts/${post._id}/`">{{
+            post.title
+          }}</router-link>
+        </h4>
+        <div class="row my-3 align-items-center">
+          <div
+            v-if="post.image && typeof post.image !== 'string'"
+            class="col-4"
+          >
+            <img :src="post.image" :alt="post.title" class="rounded-lg w-100" />
+          </div>
+          <p :class="{ 'col-8': post.image }" class="text-muted">
+            {{ post.excerpt }}
+          </p>
+        </div>
+        <span class="text-muted">{{ post.createdAt }}</span>
+      </div>
+    </article>
+  </div>
+</template>
+
+<script lang="ts">
+import { PostProps } from '@/views/column-detail/index.vue'
+import { defineComponent, PropType } from 'vue'
+
+export default defineComponent({
+  name: 'PostList',
+  props: {
+    posts: {
+      required: true,
+      type: Array as PropType<PostProps[]>
+    }
+  },
+  setup() {
+    return {}
+  }
+})
+</script>
+
+<style scoped></style>
--- src/views/column-detail/index.vue
+++ src/views/column-detail/index.vue
@@ -1,22 +1,78 @@
 <template>
-  <div>
-    <pre>{{ route }}</pre>
+  <div class="column-detail-page w-75 mx-auto">
+    <div
+      class="column-info row mb-4 border-bottom pb-4 align-items-center"
+      v-if="column"
+    >
+      <div class="col-3 text-center">
+        <img
+          :src="column.avatar"
+          :alt="column.title"
+          class="rounded-circle border w-100"
+        />
+      </div>
+      <div class="col-9">
+        <h4>{{ column.title }}</h4>
+        <p class="text-muted">{{ column.description }}</p>
+      </div>
+    </div>
+    <PostList :posts="posts" />
   </div>
 </template>
 
 <script lang="ts">
+import { ColumnListProps } from '@/components/column-list/index.vue'
 import { defineComponent } from 'vue'
 import { useRoute } from 'vue-router'
+import PostList from '@/components/post-list/index.vue'
+
+export interface PostProps {
+  _id: number
+  title: string
+  content: string
+  image?: string
+  excerpt?: string
+  createdAt: string
+  columnId: number
+}
+
+const posts: PostProps[] = [
+  {
+    _id: 3,
+    title: '标题',
+    content: '描述信息',
+    image: 'https://images.dog.ceo/breeds/terrier-silky/n02097658_7970.jpg',
+    excerpt: '123',
+    createdAt: '2021-10-10',
+    columnId: 1
+  },
+  {
+    _id: 33,
+    title: '标题',
+    content: '描述信息',
+    image: 'https://images.dog.ceo/breeds/terrier-silky/n02097658_7970.jpg',
+    createdAt: '2021-10-10',
+    columnId: 2
+  }
+]
+const column: ColumnListProps = {
+  _id: 4,
+  title: '标题2',
+  avatar: 'https://images.dog.ceo/breeds/terrier-silky/n02097658_7970.jpg',
+  description: '描述信息2'
+}
 
 export default defineComponent({
+  name: 'ColumnDetail',
   setup() {
     const route = useRoute()
     console.log('route >>> ', route.params.id)
-
     return {
-      route
+      column,
+      posts
     }
-  }
+  },
+  components: { PostList }
 })
 </script>
 
