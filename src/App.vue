7ff0149ea41cd51a5f2b9258aae10f754fbf0585
kongjun0320 <1741142595@qq.com>
2021-12-19 21:11:16 +0800

    feat: loader component

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="container">
     <GlobalHeader :user="currentUser" />
+    <Loader v-if="isLoading" />
     <router-view />
     <GlobalFooter />
   </div>
@@ -13,18 +14,22 @@ import GlobalHeader from './components/global-header/index.vue'
 import GlobalFooter from './components/global-footer/index.vue'
 import { useStore } from 'vuex'
 import { GlobalDataProps } from './store'
+import Loader from './components/loader/index.vue'
 
 export default defineComponent({
   name: 'App',
   components: {
     GlobalHeader,
-    GlobalFooter
+    GlobalFooter,
+    Loader
   },
   setup() {
     const store = useStore<GlobalDataProps>()
     const currentUser = computed(() => store.state.user)
+    const isLoading = computed(() => store.state.loading)
     return {
-      currentUser
+      currentUser,
+      isLoading
     }
   }
 })
